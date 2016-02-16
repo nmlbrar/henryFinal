8bd1bf6e6bce2f321c8501376f9c6f9b929bbec2
nmlbrar <anmolbrar288@gmail.com>
2016-02-16 11:29:39 -0800

    users linked with the postings

--- model/postingdb.php
+++ model/postingdb.php
@@ -4,13 +4,14 @@ include("connection.php");
 function post_order(){
     //insert info in the users table
     global $db;    
-    $query = "INSERT INTO postings(description, destination, currentPlace) VALUES ('".$_POST['description']."','".$_POST['destination']."','".$_POST['currentPlace']."')";
+    $query = "INSERT INTO postings(description, destination, currentPlace, user_id) VALUES ('".$_POST['description']."','".$_POST['destination']."','".$_POST['currentPlace']."','".$_POST['user_id']."')";
+    echo $query;
     $result = $db->query($query);
 }
 
 function get_orders(){
     global $db;
-    $query = "SELECT postings.description, postings.destination, postings.currentPlace FROM postings
+    $query = "SELECT postings.description, postings.destination, postings.currentPlace, postings.user_id FROM postings
                 LEFT JOIN users ON users.id = postings.user_id";
     $result = $db->query($query);
     echo json_encode($result->fetchAll());
